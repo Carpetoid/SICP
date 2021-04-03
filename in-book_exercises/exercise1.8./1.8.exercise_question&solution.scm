09ed10e7e9a89d59a7eda1a136c2b2dfef3dbe5c
Carpetoid <abdulrahmanfayoumi2@gmail.com>
2021-04-03 03:01:05 +0300

    ...

--- in-book_exercises/exercise1.8./1.8.exercise_question&solution.scm
+++ in-book_exercises/exercise1.8./1.8.exercise_question&solution.scm
@@ -1,5 +1,6 @@
 #lang sicp
-#| Question: Newton's method for cube roots is based on the fact that if y is an
+#| Exercise 1.8. problem:
+ Newton's method for cube roots is based on the fact that if y is an
 approximation to the cube root of x, then a better approximation is given by the value (x/y^2 + 2y)/3
 Use this formula to implement a cube-root procedure analogous to the square-root procedure.
 (In section 1.3.4 we will see how to implement Newton's method in general as an abstraction of these square-root
