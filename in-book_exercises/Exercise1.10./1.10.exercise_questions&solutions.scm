0910c215cda128bd30f481028b7075a261319aaf
Carpetoid <abdulrahmanfayoumi2@gmail.com>
2021-04-07 01:23:02 +0300

    exercise 1.10

--- in-book_exercises/Exercise1.10./1.10.exercise_questions&solutions.scm
+++ in-book_exercises/Exercise1.10./1.10.exercise_questions&solutions.scm
@@ -1,6 +1,6 @@
 #lang sicp
 
-; Exercise 1.10. problem:
+; Exercise 1.10. problems:
 ; The following procedure computes a mathematical function called Ackermann's function.
 
 (define (A x y)
@@ -34,7 +34,7 @@
 ; Solutions:
 
 ; Problem 1:
-; To make it easier to answer problem 2, I will answer problem 1 by using the substitution model. 
+; To make it easier to answer problem 2, I will answer problem 1's (A 1 10) part by using the substitution model. 
 
 ; 1. (A 1 10) evaluates to:
 
@@ -59,18 +59,19 @@
 ; (A 0 512)
 ; 1024
 
-; 2. (A 2 4) evaluates to:
+; 2. (A 2 4) evaluates to: 65536
 
-; (A 2 4)
-; (A 1 (A 2 3))
-; (A 1 (A 1 (A 2 2)))
-; (A 1 (A 1 (A 1 (A 2 1))))
-; (A 1 (A 1 (A 1 2)))
-; (A 1 (A 1 (A 0 (A 1 1))))
-; (A 1 (A 1 (A 0 (A 1 1) )))
+; 3. (A 3 3) evaluates to : 65536
 
-(define (A x y)
-  (cond ((= y 0) 0)
-        ((= x 0) (* 2 y))
-        ((= y 1) 2)
-        (else (A (- x 1) (A x (- y 1))))))
+; Problem 2:
+
+; 1. (define (f n) (A 0 n)), (f n): 2n
+
+; 2. (define (g n) (A 1 n)), (g n): 2^n
+
+; 3. (define (h n) (A 2 n)), (h n): 2^2^2^2^2... where the number of times 2 is raised to 2 is n times for n > 1
+
+
+
+
+
