4ecc6c4e08099d7bd7200de7575e3b4a35bccba2
Carpetoid <abdulrahmanfayoumi2@gmail.com>
2021-10-14 21:02:13 +0300

    fix

--- in-book_exercises/exercise1.3./1.3.exercise_solution.scm
+++ in-book_exercises/exercise1.3./1.3.exercise_solution.scm
@@ -1,20 +1,24 @@
 #lang sicp
 
+
+
 (define (sum_sqr_larger2 x y z)
-  (if
-   (> x y)
-   (+ (* x x)
-      (if
-       (> y z)
-       (* y y)
-       (* z z))
+  (if (> x y)
+      (+
+       (* x x)
+       (if
+        (> y z)
+        (* y y)
+        (* z z)))
       (+
        (* y y)
-         (if
-          (> x z)
-          (* x x)
-          (* z z))))))
+       (if
+        (> x z)
+        (* x x)
+        (* z z)))))
 
 
 
-(sum_sqr_larger2 2 4 3)                         
+
+
+                    
